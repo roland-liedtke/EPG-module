3c449c9f29b2347cc42062c8449d5af2ae08637a
Roland Liedtke <93646270+roland-liedtke@users.noreply.github.com>
2023-12-03 14:01:22 +0100

    Add files via upload

---
+++ FullSize/epg.js
@@ -0,0 +1,114 @@
+async function getData() {
+    
+    // łączy gałąź z HTML
+    const target = document.getElementById("target");
+    const plugin = document.getElementById("plugin");
+    
+    // ukrywa box na innych stronach
+    if (target !== null) {
+        plugin.style.display = "block";
+    }
+    
+    // zamienia destination na plugin
+    target.replaceWith(plugin);
+    
+    // pobiera dane przez API i czeka az cały się załaduje
+    const response = await fetch("https://app.mediatool.tv/api/epg-api/information/channel/tbpl/weeks/4?token=86e89a49-9e7c-48da-8c29-7db6807e8f8b");
+
+    // parsuje dane do JSON i czeka az się wykona
+    const epg = await response.json();
+
+    // łączy gałąź z HTML
+    const thead = document.getElementById("thead");
+    const tbody = document.getElementById("tbody");
+
+    // tworzy daty 
+    const today = new Date();
+    const tommorow = new Date(today);
+    tommorow.setDate(today.getDate() + 1);
+
+    // funkcja dopisuje "0", 6:00 => 06:00
+    function addZero(x) {
+        if (x < 10) { x = "0" + x }
+        return x;
+    }
+
+    // dodaje divy w nałówku tabeli
+    thead.innerHTML += `<a class="box" id="todayBox">Dziś ${addZero(today.getDate())}/${addZero(today.getMonth() + 1)}</a>`;
+    thead.innerHTML += `<a class="box" id="tommorowBox">Jutro ${addZero(tommorow.getDate())}/${tommorow.getMonth() + 1}</a>`;
+
+    // wyświetla domyślnie dane z Dzisiaj 
+    showToday();
+    
+    // nasłuchuje na kliknięcie i wykonuje odpowiednią funkcję
+    document.getElementById("todayBox").addEventListener("click", showToday);
+    document.getElementById("tommorowBox").addEventListener("click", showTommorow);
+
+
+    function showToday() {
+
+        // czyści listę
+        tbody.innerHTML = "";
+
+        // zmienia style aktywnego filtra w nagłówku
+        tommorowBox.style.backgroundColor = "transparent";
+        const todayBox = document.getElementById("todayBox");
+        todayBox.style.backgroundColor = "rgb(239, 34, 44)";
+
+        // iteruje po wszystkich elementach schedule
+        epg.schedule.forEach(element => {
+
+            let startTime = new Date(element.startTime);
+            let endTime = new Date(element.endTime);
+            let now = new Date();
+
+            if (startTime.getDate() == today.getDate()) {
+                tbody.innerHTML += `<tr>
+                    <td>
+                        ${addZero(startTime.getHours())}:${addZero(startTime.getMinutes())} <br>
+                        ${(startTime <= now && now <= endTime) ? `<span class="live">Teraz</span>` : ""}
+                    </td>
+                    <td>
+                        ${element.title}<br><span>${element.episodeTitle['AB'] == undefined ? "" : element.episodeTitle['AB']}</span>
+                    </td>
+                </tr>`
+            }
+        });
+    }
+
+    function showTommorow() {
+
+        // czyści listę
+        tbody.innerHTML = "";
+
+        // zmienia style aktywnego filtra w nagłówku
+        const tomorrowBox = document.getElementById("tommorowBox");
+        todayBox.style.backgroundColor = "transparent";
+        tomorrowBox.style.backgroundColor = "rgb(239, 34, 44)";
+        
+        // iteruje po wszystkich elementach schedule
+        epg.schedule.forEach(element => {
+
+            let time = new Date(element.startTime);
+
+            // funkcja dopisuje "0", 6:00 => 06:00
+            function addZero(i) {
+                if (i < 10) { i = "0" + i }
+                return i;
+            }
+
+            if (time.getDate() == tommorow.getDate()) {
+                tbody.innerHTML += `<tr>
+                <td>
+                    ${addZero(time.getHours())}:${addZero(time.getMinutes())}
+                </td>
+                <td>
+                    ${element.title}<br><span>${element.episodeTitle['AB'] == undefined ? "" : element.episodeTitle['AB']}</span>
+                </td>
+            </tr>`
+            }
+        });
+    }
+}
+
+getData();
---
+++ Short/short.js
@@ -0,0 +1,60 @@
+async function getShort() {
+
+    // łączy gałąź z HTML
+    const target = document.getElementById("SHORT");
+    const shortSchedule = document.getElementById("shortSchedule");
+
+    // ukrywa box na innych stronach
+    if (target !== null) {
+        shortSchedule.style.display = "flex";
+    }
+
+    // zamienia destination na plugin
+    target.replaceWith(shortSchedule);
+
+    // pobiera dane przez API i czeka az cały się załaduje
+    const response = await fetch("https://app.mediatool.tv/api/epg-api/information/channel/tbpl/weeks/4?token=86e89a49-9e7c-48da-8c29-7db6807e8f8b");
+
+    // parsuje dane do JSON i czeka az się wykona
+    const epg = await response.json();
+
+    // dodaje pierwszy szary box
+    shortSchedule.innerHTML = `<div class="program"><p>Program TV</p></div>`
+
+    // index aktualnego programu 
+    let nowPlayingIndex = "";
+    let shortList = [];
+    
+
+    epg.schedule.forEach(element => {
+
+        const startTime = new Date(element.startTime);
+        const endTime = new Date(element.endTime);
+        const now = new Date();
+
+        if (startTime <= now && endTime >= now) {
+            nowPlayingIndex = epg.schedule.indexOf(element);
+            shortList.push(element);
+            shortList.push(epg.schedule[nowPlayingIndex + 1])
+            shortList.push(epg.schedule[nowPlayingIndex + 2])
+        }
+    });
+
+    for (let i = 0; i < 3; i++) {
+
+        function addZero(x) {
+            if (x < 10) { x = "0" + x }
+            return x;
+        }
+
+        const title = shortList[i].title;
+        const startTime = new Date(shortList[i].startTime);
+        const endTime = new Date(shortList[i].endTime);
+
+        shortSchedule.innerHTML += `<div class="item"><div class="time">${addZero(startTime.getHours())}:${addZero(startTime.getMinutes())} - ${addZero(endTime.getHours())}:${addZero(endTime.getMinutes())}</div><span class="title">${title}</span></div>`;
+    }
+
+    shortSchedule.innerHTML += `<div><a href="https://telewizjabiznesowa.pl/program-tv/" class="more">Zobacz więcej</a></div>`
+}
+
+getShort();
